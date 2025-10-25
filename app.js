98b9c789ea2a9407078b2159654c423e758d4178
testteachedk1! <akenessy@yandex.ru>
2025-10-25 16:56:16 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+ let firstNumber = 1; //задаём первое число
+ let secondNumber = 10;//задаём второе число
+
+ if (firstNumber > secondNumber) { //проверяем больше ли первое число число
+    console.log("Наибольшее число = ", firstNumber``)//если больше выводим 
+ } else { //если меньше первое число
+    console.log("Наибольшее число = ", secondNumber); //значит второе больше, выводим его
+ }
