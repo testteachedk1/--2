f256495fdcb7a9336c68fb5ae6f3bfd4bf444d9e
akenessy <zraev2004@gmail.com>
2025-11-01 18:24:15 +1000

    lesson5

--- app.js
+++
@@ -1,8 +0,0 @@
- let firstNumber = 1; //задаём первое число
- let secondNumber = 10;//задаём второе число
-
- if (firstNumber > secondNumber) { //проверяем больше ли первое число число
-    console.log("Наибольшее число = ", firstNumber``)//если больше выводим 
- } else { //если меньше первое число
-    console.log("Наибольшее число = ", secondNumber); //значит второе больше, выводим его
- }
